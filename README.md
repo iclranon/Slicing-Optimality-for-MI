81740433d3315b24e238b6994c1a9221a6bf6aa4
iclranon <114503638+iclranon@users.noreply.github.com>
2022-09-27 11:31:34 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Distributional Sliced Information
+
+<img src="https://render.githubusercontent.com/render/math?math=\tau (X,Y)=\sup\oint{I}(\theta^TX,\phi^TY)d\sigma(\theta,\phi)">
+
+This is the code accompanying the paper titled 'Distributional Sliced Information: Theory with Applications to Machine
+Learning'. The full code will be published with the camera-ready version.
---
+++ main.py
@@ -0,0 +1,23 @@
+from DSI import DSI
+import torch
+import numpy as np
+import math
+
+def sample_correlated_gaussian(rho=0.5, dim=20, batch_size=128, cubic=None):
+    """Generate samples from a correlated Gaussian distribution."""
+    x, eps = torch.chunk(torch.randn(batch_size, 2 * dim), 2, dim=1)
+    y = rho * x + torch.sqrt(torch.tensor(1. - rho**2).float()) * eps
+
+    if cubic is not None:
+        y = y ** 3
+
+    return x, y
+
+
+if __name__ == '__main__':
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    rho=.9
+    X,Y=sample_correlated_gaussian(rho=rho,dim=1,batch_size=500)
+    print(DSI(X,Y,num_slices=100))
